4fe367a2240a60d06a8ba14916dc3b8b835d9363
David Foote <david.a.foote@gmail.com>
2019-06-10 03:47:26 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,9 +1,12 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(names)
+  index = 0
+  names.each_with_index {|name, index| puts "#{index + 1}. #{name} "}
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(planeteer_calls)
+  planeteer_calls = ["earth", "wind", "fire"]
+  planeteer_calls.collect! {|planeteer_calls| planeteer_calls.capitalize}
+  planeteer_calls.each { |planeteer_calls| puts planeteer_calls + '!' }
 end
 
 def long_planeteer_calls# code an argument here
